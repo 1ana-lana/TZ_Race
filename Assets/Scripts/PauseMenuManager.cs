cc0f231fde60bdff0f43ce7b107b3b5cadde97e0
Svitlana Andrieieva <svitlana0andrieieva@gmail.com>
2024-09-08 23:01:34 +0300

    add pause menu

--- Assets/Scripts/PauseMenuManager.cs
+++ Assets/Scripts/PauseMenuManager.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public event Action OnRestartGame;
+
+    [SerializeField]
+    private Button _restart;
+    [SerializeField]
+    private Button _exit;
+    [SerializeField]
+    private Button _closePanel;
+
+    private void Awake()
     {
-        
+        _restart.onClick.AddListener(RestartGame);
+        _exit.onClick.AddListener(ExitGame);
+        _closePanel.onClick.AddListener(ClosePanel);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ExitGame()
     {
-        
+        Application.Quit();
+    }
+
+    private void RestartGame()
+    {
+        OnRestartGame?.Invoke();
+    }
+
+    private void ClosePanel()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1;
     }
 }
